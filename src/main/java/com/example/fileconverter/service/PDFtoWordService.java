75134bf3a58359f8dcc9647784c1bd0cec907f86
Porpi01 <macanyiz@gmail.com>
2025-09-19 12:27:24 +0200

    dependencias ApachePoi

--- src/main/java/com/example/fileconverter/service/PDFtoWordService.java
+++ src/main/java/com/example/fileconverter/service/PDFtoWordService.java
@@ -1,5 +1,8 @@
 package com.example.fileconverter.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PDFtoWordService {
 
 }
--- src/main/java/com/example/fileconverter/service/WordtoPDFService.java
+++ src/main/java/com/example/fileconverter/service/WordtoPDFService.java
@@ -1,5 +1,12 @@
 package com.example.fileconverter.service;
 
+import org.springframework.stereotype.Service;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+@Service
 public class WordtoPDFService {
+
+  
 
 }
